022760043e75294f1ae6e8465b9e5c3611ecbabb
sgalutira <sheena.galutira@gmail.com>
2016-07-14 22:05:20 -1000

    gallery new

--- server.js
+++ server.js
@@ -3,48 +3,58 @@ var pug = require('pug');
 var querystring = require('querystring');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
 
 var Gallery = require('./Gallery');
+
+var pics = './data/gallery.json';
+
 
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
+
   res.render('index');
-  // response.send('List of gallery photos');
+
 });
 
 // app.get('/gallery/:id', function(req, res){
   app.get('/gallery/:id(\\d+)/', function (req, res){
-  res.send('gallery id: ' + req.params.id);
+
+    // res.send('gallery id: ' + req.params.id);
 });
 
 app.get('/gallery/new', function(req, res){
-
-  res.send('New gallery');
+  res.render('gallery');
+  // res.send('New gallery');
 });
 
-  app.post('/gallery', function(req, res){
-    req.on('data', function(data){
-      var locals = querystring.parse(data.toString());
-      Gallery.create(locals, function (err, result){
-        if (err){
-          throw err;
-        }
-        res.render('gallery', locals);
-      });
+app.post('/gallery', function(req, res){
+  req.on('data', function(data){
+    var locals = querystring.parse(data.toString());
+    Gallery.create(locals, function (err, result){
+      if (err){
+        throw err;
+      }
+      res.render('gallery', locals);
     });
   });
+});
 
-// app.post('/gallery', function(req, res){
-//   req.on('data', function(data){
-//     var sentData = data.toString();
-//     var reqData = querystring.parse(sentData);
-//     console.log('Gallery req: ', reqData);
-//   });
-// });
+
 
 app.get('/gallery/:id/edit', function(req, res){
+
+});
+
+app.put('/gallery/:id/', function(req, res){
+
+});
+
+app.delete('/gallery/:id', function (req, res){
 
 });
 
