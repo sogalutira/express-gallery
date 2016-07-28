587616544d5da4ac3f913cc161863603a0e169f8
sgalutira <sheena.galutira@gmail.com>
2016-07-27 22:01:08 -1000

    sequelize created models

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,9 +1,15 @@
+'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Gallery = sequelize.define("Gallery", {
+  var Gallery = sequelize.define('Gallery', {
     author: DataTypes.STRING,
     url: DataTypes.STRING,
     description: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
   });
-
   return Gallery;
 };
