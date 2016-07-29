2c537770928b74fa8da014095db641c876a1c566
sgalutira <sheena.galutira@gmail.com>
2016-07-28 23:02:16 -1000

    POST WERKSS

--- server.js
+++ server.js
@@ -29,11 +29,6 @@ app.use(methodOverride(function(req, res){
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// app.get('/', function(req, res){
-//   var galleryJSON = require('./data/gallery');
-//   res.render('index', {galleries: galleryJSON});
-// });
-
 app.get('/', function(req, res){
     Gallery.findAll()
   .then(function (gallery){
@@ -41,20 +36,10 @@ app.get('/', function(req, res){
   });
 });
 
-
-// // app.get('/gallery/:id', function(req, res){
-//   app.get('/gallery/:id(\\d+)/', function (req, res){
-//     var idObj = [];
-//     var galleryJSON = require('./data/gallery');
-//     var id = req.params.id;
-//     console.log('gallery stuff: ', galleryJSON[id]);
-//     var theIds = idObj.push(galleryJSON[id]);
-//     console.log('line 29');
-//     console.log(theIds);
-
-//     res.render('singlegallery', {galleries: theIds});
-//     // res.send('gallery id: ' + req.params.id);
-// });
+app.get('/gallery/new', function(req, res){
+  res.render('gallery');
+  // res.send('New gallery');
+});
 
 app.get('/gallery/:id', function(req, res){
   Gallery.findOne({
@@ -65,52 +50,44 @@ app.get('/gallery/:id', function(req, res){
   });
 });
 
-app.get('/gallery/new', function(req, res){
-  res.render('gallery');
-  // res.send('New gallery');
-});
-
-// app.post('/gallery', locals, function(req, res){
-//     Gallery.create(req.body, function (err, result){
-//       console.log(req.body);
-//       if (err){
-//         throw err;
-//       }
-//       res.render('gallery', req.body);
-//     });
-// });
-
-app.post('/gallery', function(req, res){
-  Gallery.create({ author: req.body.author, url: req.body.url, description: req.body.description })
-    .then(function (gallery) {
-      res.render('gallery', gallery);
-    });
-});
-
 app.get('/gallery/:id/edit', function(req, res){
     Gallery.findOne({
       where: { id: req.params.id}
     })
-  //   .then(function (gallery){
-  //   res.render('editgallery', {gallery: gallery});
-  // });
-    // Gallery.update( {author: req.body.author, url: req.body.url, description: req.body.description}, {where: { id: req.params.id}} )
     .then(function (edit){
       res.render('editgallery', {gallery: edit});
     });
 });
 
+app.post('/gallery', function(req, res, next){
+  Gallery.create({
+    author: req.body.author,
+    url: req.body.url,
+    description: req.body.description
+  })
+  .then(function (gallery) {
+    res.render('singlegallery', {gallery: gallery});
+  });
+});
+
 app.put('/gallery/:id', function(req, res){
-  Gallery.update( {author: req.body.author, url: req.body.url, description: req.body.description}, {where: { id: req.params.id}} )
-    .then(function(){
-       return Gallery.findOne({
-        where: { id: req.params.id}
-        });
-    })
-    .then(function (update){
-      res.render('singlegallery', {gallery: update});
-      // res.redirect('singlegallery');
+  Gallery.update( {
+    author: req.body.author,
+    url: req.body.url,
+    description: req.body.description
+  }, {
+    where: {
+      id: req.params.id
+    }} )
+  .then(function(){
+     return Gallery.findOne({
+      where: { id: req.params.id}
     });
+  })
+  .then(function (update){
+    res.render('singlegallery', {gallery: update});
+    // res.redirect('singlegallery');
+  });
 });
 
 app.delete('/gallery/:id', function (req, res){
@@ -119,9 +96,9 @@ app.delete('/gallery/:id', function (req, res){
       id: req.params.id
     },
     truncate: true})
-    .then(function(Gallery){
-      res.redirect('/');
-    });
+  .then(function(Gallery){
+    res.redirect('/');
+  });
 });
 
 var server = app.listen(8080, function(){
