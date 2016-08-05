e3f57a5ac94ee61a2b7d2f60064e02c8c6ca3e15
sgalutira <sheena.galutira@gmail.com>
2016-08-04 21:17:40 -1000

    authentication fixed

--- server.js
+++ server.js
@@ -1,51 +1,109 @@
+var bodyParser = require('body-parser');
 var express = require('express');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+var passport = require('passport');
+var path = require('path');
 var pug = require('pug');
 var querystring = require('querystring');
-var path = require('path');
-var app = express();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 var session = require('express-session');
-var passport = require('passport');
+var redis = require('connect-redis');
+
+var RedisStore = redis(session);
 var LocalStrategy = require('passport-local').Strategy;
+var app = express();
 
 var db = require('./models');
 var Gallery = db.Gallery;
+var Users = db.Users;
 var locals = bodyParser.urlencoded({ extended: false });
 
-app.use(morgan('dev'));
-app.use(locals);
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.resolve(__dirname, "public")));
+app.use(locals);
+app.use(morgan('dev'));
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done){
-  done(null, user);
-});
-
-// var user = { username: 'bob', password: 'secret', email: 'bob@example.com' };
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // var USERNAME = CONFIG.CONFIG.USERNAME;
-    // var PASSWORD = CONFIG.CONFIG.PASSWORD;
-    // if (username === user.username && password === user.password){
-    //   return done(null, {});
-    Users.findOne({where: {username: username}})
-    .then(function(user){
-      if(user){
-        return done(null, user);
-      }
-  });
+app.use(session({
+  store: new RedisStore(),
+  secret: 'keyboardcat',
+  resave: true,
+  saveUninitilized: false
 }));
+
+
+// passport.deserializeUser(function(id, done){
+//   Users.findById(id, function(err, user){
+//     done(err, user);
+//   });
+// });
+
+// function authenticate(username, password){
+//   Users.findOne({
+//     where: {
+//       username: username,
+//       password: password
+//     }
+//   })
+//   .then(function(user){
+//     if(user){
+//       return done(null, user);
+//     }
+//   });
+// }
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Users.findOne({
+      where: {
+        username: username,
+        password: password
+      }
+    })
+    .then(function(user){
+      if (user === null) {
+        return done(null, false);
+      }
+      // console.log(username);
+      // console.log(password);
+      // if (!(username === user.username && password === user.password)){
+      //   console.log('after if statement false');
+      //   return done(null, false);
+      // }
+      console.log('before return user');
+      return done(null, user); // kicks off serializeUser
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(userId, done) {
+  return Users.findById(userId)
+    .then(function(user){
+      return done(null, user);
+    })
+    .catch(function(err){
+      return done(err);
+    });
+  });
+//   Users.findOne({
+//     where: {
+//       id: userId
+//     }
+//   })
+//   .then(function(user) {
+//     done(null, user);
+//   });
+// });
 
 app.use(methodOverride(function(req, res){
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
@@ -53,11 +111,23 @@ app.use(methodOverride(function(req, res){
     var method = req.body._method;
     delete req.body._method;
     return method;
-  }
+  } console.log('after method override');
 }));
 
-app.set('views', path.resolve(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+
+app.get('/secret', function(req, res){
+  res.render('secret');
+});
 
 app.get('/', function(req, res){
   Gallery.findAll()
@@ -66,45 +136,39 @@ app.get('/', function(req, res){
   });
 });
 
-app.get('/login', function(req, res){
-  res.render('login');
+app.get('/gallery', function(req, res){
+  res.redirect('/');
 });
 
-app.post('/login', passport.authenticate('local',
-  {successRedirect: '/secret',
-  failureRedirect: '/login'
-}));
-
-app.get('/secret', function(req, res){
-  res.render('secret');
-});
-
-app.get('/gallery/new', passport.authenticate('local', {session:false}), function(req, res){
-  console.log(req.user);
+app.get('/gallery/new', isAuthenticated, function(req, res){
   res.render('gallery');
 });
 
 app.get('/gallery/:id', function(req, res){
   Gallery.findOne({
-    where: { id: req.params.id}
+    where: {
+      id: req.params.id
+    }
   })
   .then(function (gallery){
     res.render('singlegallery', {gallery: gallery});
   });
 });
 
-app.get('/gallery/:id/edit', function(req, res){
+app.get('/gallery/:id/edit', isAuthenticated, function(req, res){
     Gallery.findOne({
-      where: { id: req.params.id}
+      where: {
+        id: req.params.id
+      }
     })
     .then(function (edit){
       res.render('editgallery', {gallery: edit});
     });
 });
 
-app.get('/gallery', function(req, res){
-  res.redirect('/');
-});
+// app.post('/login', function(req, res) {
+//   console.log("entered the app.post method");
+// });
 
 app.post('/gallery', function(req, res, next){
   Gallery.create({
@@ -118,7 +182,7 @@ app.post('/gallery', function(req, res, next){
 });
 
 app.put('/gallery/:id', function(req, res){
-  Gallery.update( {
+  Gallery.update({
     author: req.body.author,
     url: req.body.url,
     description: req.body.description
@@ -156,3 +220,10 @@ var server = app.listen(8080, function(){
   console.log('Listening on: ', host, port);
   db.sequelize.sync();
 });
+
+function isAuthenticated (req, res, next) {
+  if(!req.isAuthenticated()){
+    return res.redirect('/login');
+  }
+  return next();
+}
